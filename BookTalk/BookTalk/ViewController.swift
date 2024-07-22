08db91a55376a0a9fdb79d740efbc2852d0bbb1a
Mini <minnnidev@gmail.com>
2024-07-22 22:14:24 +0900

    [feat] 프로젝트 생성

---
+++ BookTalk/BookTalk/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BookTalk
+//
+//  Created by 김민 on 7/22/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+    }
+
+
+}
+
