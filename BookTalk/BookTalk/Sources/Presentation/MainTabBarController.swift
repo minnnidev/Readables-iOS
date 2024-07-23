efcb934c8cd3ad18e375109b45315240d8e2addb
RAFA <rafa71802937@gmail.com>
2024-07-23 23:27:29 +0900

    [add] 각 탭에 해당하는 뷰 컨트롤러 생성
    - 뷰와 관련된 파일은 Presentation 폴더 안으로 이동

---
+++ BookTalk/BookTalk/Sources/Presentation/MainTabBarController.swift
@@ -0,0 +1,29 @@
+//
+//  MainTabBarController.swift
+//  BookTalk
+//
+//  Created by RAFA on 7/23/24.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
