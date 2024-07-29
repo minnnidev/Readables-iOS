a37d73f3a84e8a76f418193f56d3f587890c2c7b
RAFA <rafa71802937@gmail.com>
2024-07-29 12:48:03 +0900

    [feat] 데이터 모델 생성

---
+++ BookTalk/BookTalk/Sources/Model/HomeBooks.swift
@@ -0,0 +1,14 @@
+//
+//  HomeBooks.swift
+//  BookTalk
+//
+//  Created by RAFA on 7/29/24.
+//
+
+import UIKit
+
+struct HomeBooks {
+    let title: String
+    let author: String
+    let coverImage: UIImage?
+}
