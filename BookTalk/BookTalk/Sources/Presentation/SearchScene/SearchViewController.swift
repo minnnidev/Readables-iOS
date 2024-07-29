8c57588044bc09e56c687b01e56715121e547756
RAFA <rafa71802937@gmail.com>
2024-07-29 17:24:56 +0900

    [feat] SearchBar UI 구현

--- BookTalk/BookTalk/Sources/Presentation/SearchScene/SearchViewController.swift
+++ BookTalk/BookTalk/Sources/Presentation/SearchScene/SearchViewController.swift
@@ -7,10 +7,55 @@
 
 import UIKit
 
-class SearchViewController: UIViewController {
+final class SearchViewController: BaseViewController {
+    
+    // MARK: - Properties
+    
+    private let searchBar = UISearchBar()
+    
+    // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tabBarController?.tabBar.isHidden = true
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        searchBar.becomeFirstResponder()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        tabBarController?.tabBar.isHidden = false
+    }
+    
+    // MARK: - Actions
+    
+    @objc private func cancelButtonDidTap() {
+        searchBar.resignFirstResponder()
+    }
+    
+    // MARK: - Set UI
+    
+    override func setNavigationBar() {
+        searchBar.placeholder = "책 이름 / 작가 이름"
+        navigationItem.titleView = searchBar
+        
+        let cancel = UIBarButtonItem(
+            barButtonSystemItem: .cancel,
+            target: self,
+            action: #selector(cancelButtonDidTap)
+        )
+        
+        navigationItem.rightBarButtonItem = cancel
+    }
+    
+    override func setViews() {
         view.backgroundColor = .white
     }
 }
