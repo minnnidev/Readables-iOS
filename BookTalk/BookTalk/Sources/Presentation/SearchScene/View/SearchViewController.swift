a9e83c01e38f93842b74b56612b2d3d0c6f35441
RAFA <rafa71802937@gmail.com>
2024-07-31 16:58:31 +0900

    [feat] 검색 결과 UI 구현
    - keyboardDismissMode = .interactive -> 위로 스크롤 시 키보드 자동으로 dismiss
    - 소문자로도 검색 필터 가능
    - 키보드가 테이블 뷰 최하단 가리지 않도록 설정

--- BookTalk/BookTalk/Sources/Presentation/SearchScene/View/SearchViewController.swift
+++ BookTalk/BookTalk/Sources/Presentation/SearchScene/View/SearchViewController.swift
@@ -11,36 +11,144 @@ final class SearchViewController: BaseViewController {
     
     // MARK: - Properties
     
+    private let viewModel = SearchViewModel()
+    private let tableView = UITableView(frame: .zero, style: .plain)
     private let searchBar = UISearchBar()
+    private var inSearchMode: Bool {
+        return !(searchBar.text?.isEmpty ?? true)
+    }
     
     // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        bind()
+        setupKeyboardNotifications()
     }
     
-    // MARK: - Actions
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
     
-    @objc private func cancelButtonDidTap() {
-        searchBar.resignFirstResponder()
+    // MARK: - Bind
+    
+    private func bind() {
+        viewModel.onBooksUpdated = { [weak self] in
+            self?.tableView.reloadData()
+        }
+    }
+    
+    // MARK: - Helpers
+    
+    private func setupKeyboardNotifications() {
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(handleKeyboardWillShow(notification:)),
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil
+        )
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(handleKeyboardWillHide(notification:)),
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil
+        )
+    }
+    
+    @objc private func handleKeyboardWillShow(notification: NSNotification) {
+        guard let userInfo = notification.userInfo,
+              let keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect
+        else {
+            return
+        }
+        
+        let bottomSafeAreaInset = view.safeAreaInsets.bottom
+        let keyboardHeight = keyboardFrame.height - bottomSafeAreaInset
+        tableView.contentInset.bottom = keyboardHeight
+        tableView.verticalScrollIndicatorInsets.bottom = keyboardHeight
+    }
+    
+    @objc private func handleKeyboardWillHide(notification: NSNotification) {
+        tableView.contentInset.bottom = 0
+        tableView.verticalScrollIndicatorInsets.bottom = 0
     }
     
     // MARK: - Set UI
     
     override func setNavigationBar() {
+        searchBar.delegate = self
+        searchBar.autocapitalizationType = .none
+        searchBar.autocorrectionType = .no
+        searchBar.spellCheckingType = .no
         searchBar.placeholder = "책 이름 / 작가 이름"
+        searchBar.sizeToFit()
         navigationItem.titleView = searchBar
-        
-        let cancel = UIBarButtonItem(
-            barButtonSystemItem: .cancel,
-            target: self,
-            action: #selector(cancelButtonDidTap)
-        )
-        
-        navigationItem.rightBarButtonItem = cancel
     }
     
     override func setViews() {
         view.backgroundColor = .white
+        view.addSubview(tableView)
+        
+        tableView.do {
+            $0.dataSource = self
+            $0.delegate = self
+            $0.rowHeight = UITableView.automaticDimension
+            $0.estimatedRowHeight = 160
+            $0.keyboardDismissMode = .interactive
+            $0.automaticallyAdjustsScrollIndicatorInsets = false
+            $0.register(SearchCell.self, forCellReuseIdentifier: "SearchCell")
+        }
+    }
+    
+    override func setConstraints() {
+        tableView.snp.makeConstraints {
+            $0.centerX.left.bottom.equalToSuperview()
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+        }
+    }
+}
+
+// MARK: - UITableViewDataSource
+
+extension SearchViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return inSearchMode ? viewModel.filteredBooks.count : viewModel.allBooks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(
+            withIdentifier: "SearchCell",
+            for: indexPath
+        ) as? SearchCell else {
+            return UITableViewCell()
+        }
+
+        let books = inSearchMode ? viewModel.filteredBooks : viewModel.allBooks
+        
+        if indexPath.row < books.count {
+            let book = books[indexPath.row]
+            cell.selectionStyle = .none
+            cell.bind(book)
+        }
+        
+        return cell
+    }
+}
+
+// MARK: - UITableViewDelegate
+
+extension SearchViewController: UITableViewDelegate {
+}
+
+// MARK: - UISearchBarDelegate
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        viewModel.filterBooks(searchText: searchText.lowercased())
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
     }
 }
