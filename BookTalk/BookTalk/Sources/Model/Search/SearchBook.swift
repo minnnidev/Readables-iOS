27c84c66429dcbbd841405e13dcc4b390adce8e2
RAFA <rafa71802937@gmail.com>
2024-07-31 16:45:46 +0900

    [feat] 데이터 모델 생성

---
+++ BookTalk/BookTalk/Sources/Model/Search/SearchBook.swift
@@ -0,0 +1,19 @@
+//
+//  SearchBook.swift
+//  BookTalk
+//
+//  Created by RAFA on 7/31/24.
+//
+
+import Foundation
+
+struct SearchBook {
+    let coverImageURL: String
+    let title: String
+    let author: String
+    let publisher: String
+    let publicationDate: String
+    let availability: Bool
+    var isFavorite: Bool
+    var isBookmarked: Bool
+}
