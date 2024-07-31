fbefbaa2e8db1aa2749512433bcd7e2eea52509d
RAFA <rafa71802937@gmail.com>
2024-08-01 02:18:58 +0900

    [feat] 데이터 모델 구현

--- BookTalk/BookTalk/Sources/Model/BookInfo/BookInfo.swift
+++ BookTalk/BookTalk/Sources/Model/BookInfo/BookInfo.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+struct BasicBookInfo {
+    let coverImageURL: String
+    let title: String
+    let author: String
+}
+
+struct DetailBookInfo {
+    let basicBookInfo: BasicBookInfo
+    let publisher: String
+    let publicationDate: String
+    let isAvailable: Bool
+    var isFavorite: Bool
+    var distance: Double?
+}
