7b72d7c14563c5aa8e72d549fd98b91b782aa9df
Mini <minnnidev@gmail.com>
2024-08-02 02:52:47 +0900

    [style] 입력창 UI 구현

--- BookTalk/BookTalk/Sources/Presentation/OpenTalk/View/ChatViewController.swift
+++ BookTalk/BookTalk/Sources/Presentation/OpenTalk/View/ChatViewController.swift
@@ -13,6 +13,9 @@ import Then
 final class ChatViewController: BaseViewController {
 
     private let chatTableView = UITableView(frame: .zero)
+    private let textInputView = UIView()
+    private let messageTextField = UITextField()
+    private let sendButton = UIButton()
 
     private let viewModel = ChatViewModel()
 
@@ -44,16 +47,52 @@ final class ChatViewController: BaseViewController {
             $0.backgroundColor = UIColor(hex: 0xFFDCDC) // TODO: 색상 변경
             $0.separatorStyle = .none
         }
+
+        textInputView.do {
+            $0.backgroundColor = .white
+        }
+
+        messageTextField.do {
+            $0.placeholder = "대화를 입력해 주세요"
+            $0.layer.borderColor = UIColor.gray100.cgColor
+            $0.layer.borderWidth = 1
+            $0.layer.cornerRadius = 10
+        }
+
+        sendButton.do {
+            $0.setImage(UIImage(systemName: "paperplane"), for: .normal)
+            $0.tintColor = .black
+        }
     }
 
     override func setConstraints() {
-        [chatTableView].forEach {
+        [chatTableView, textInputView].forEach {
             view.addSubview($0)
         }
 
+        [messageTextField, sendButton].forEach {
+            textInputView.addSubview($0)
+        }
+
         chatTableView.snp.makeConstraints {
-            $0.top.leading.trailing.equalTo(view.safeAreaLayoutGuide)
-            $0.bottom.equalToSuperview()
+            $0.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+
+        textInputView.snp.makeConstraints {
+            $0.leading.trailing.bottom.equalTo(view.safeAreaLayoutGuide)
+            $0.height.equalTo(60)
+        }
+
+        messageTextField.snp.makeConstraints {
+            $0.leading.top.equalToSuperview().offset(8)
+            $0.trailing.equalTo(sendButton.snp.leading).offset(-8)
+            $0.bottom.equalToSuperview().offset(-8)
+        }
+
+        sendButton.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.trailing.equalToSuperview().offset(-8)
+            $0.width.height.equalTo(30)
         }
     }
 
@@ -78,18 +117,18 @@ extension ChatViewController: UITableViewDataSource {
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let random = [0, 1].randomElement()!
+        let chat = viewModel.chats[indexPath.row]
 
-        if random == 0 {
+        if chat.isMine {
             guard let cell = tableView.dequeueReusableCell(withIdentifier: OtherChatBubbleCell.identifier, for: indexPath) as? OtherChatBubbleCell else { return UITableViewCell() }
 
-            cell.bind(with: viewModel.chats[indexPath.row])
+            cell.bind(with: chat)
 
             return cell
         } else {
             guard let cell = tableView.dequeueReusableCell(withIdentifier: MyChatBubbleCell.identifier, for: indexPath) as? MyChatBubbleCell else { return UITableViewCell() }
 
-            cell.bind(with: viewModel.chats[indexPath.row].message)
+            cell.bind(with: chat.message)
 
             return cell
         }
