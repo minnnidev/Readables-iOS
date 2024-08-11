411911b957c923ce7b15400cb8d990160de2e899
Mini <minnnidev@gmail.com>
2024-08-11 17:31:13 +0900

    [style] 채팅방 즐겨찾기 토글 추가

--- BookTalk/BookTalk/Sources/Presentation/OpenTalk/ViewModel/ChatViewModel.swift
+++ BookTalk/BookTalk/Sources/Presentation/OpenTalk/ViewModel/ChatViewModel.swift
@@ -8,5 +8,25 @@
 import Foundation
 
 final class ChatViewModel {
-    var chats: [ChatModel] = [.chatStub1, .chatStub2, .chatStub3]
+
+    enum Action {
+        case loadChats
+        case toggleBookmark
+    }
+
+    var isBookmarked = Observable(false) // TODO: 임시로 false 설정
+    var chats = Observable<[ChatModel]>([])
+
+    func send(action: Action) {
+
+        switch action {
+        case .loadChats:
+            // TODO: 채팅 API 통신
+            chats.value.append(contentsOf: [.chatStub1, .chatStub2, .chatStub3])
+
+        case .toggleBookmark:
+            // TODO: 채팅방 즐겨찾기 API - 요청 성공 시 토글
+            isBookmarked.value.toggle()
+        }
+    }
 }
