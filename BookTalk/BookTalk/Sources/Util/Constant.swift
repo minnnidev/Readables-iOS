5302e3ba017c901b436394e3ab9fafa4263cf02e
Mini <minnnidev@gmail.com>
2024-08-14 00:35:08 +0900

    [add] jwt 관련 토큰 상수 추가

--- BookTalk/BookTalk/Sources/Util/Constant.swift
+++ BookTalk/BookTalk/Sources/Util/Constant.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 typealias ScreenSize = Constant.ScreenSize
+typealias TokenType = Constant.TokenType
 
 enum Constant { }
 
@@ -17,5 +18,13 @@ extension Constant {
         static let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene
         static let width = windowScene?.screen.bounds.width ?? 0
         static let height = windowScene?.screen.bounds.height ?? 0
+    }
+}
+
+extension Constant {
+
+    struct TokenType {
+        static let accessToken = "accessToken"
+        static let refreshToken = "refreshToken"
     }
 }
