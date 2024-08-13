706e06688547eb0b690b664883039e9a025251b4
Mini <minnnidev@gmail.com>
2024-08-13 16:15:56 +0900

    [style] 책 추가 UI 구현

--- BookTalk/BookTalk/Sources/Presentation/Goal/View/AddBookViewController.swift
+++ BookTalk/BookTalk/Sources/Presentation/Goal/View/AddBookViewController.swift
@@ -9,9 +9,120 @@ import UIKit
 
 final class AddBookViewController: BaseViewController {
 
+    // MARK: - Properties
+
+    private let searchBar = UISearchBar()
+    private let resultCollectionView = UICollectionView(frame: .zero, collectionViewLayout: .init())
+
+    // MARK: Initializer
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .yellow
+        setDelegate()
+        registerCell()
+    }
+
+    // MARK: - UI Setup
+
+    override func setNavigationBar() {
+        navigationItem.titleView = searchBar
+    }
+
+    override func setViews() {
+        view.backgroundColor = .white
+
+        searchBar.do {
+            $0.placeholder = "어떤 책을 읽고 계신가요?"
+        }
+
+        resultCollectionView.do {
+            let flowLayout = UICollectionViewFlowLayout()
+            flowLayout.scrollDirection = .vertical
+
+            $0.collectionViewLayout = flowLayout
+            $0.backgroundColor = .clear
+            $0.contentInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+            $0.showsVerticalScrollIndicator = false
+        }
+    }
+
+    override func setConstraints() {
+        [searchBar, resultCollectionView].forEach {
+            view.addSubview($0)
+        }
+
+        resultCollectionView.snp.makeConstraints {
+            $0.top.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+            $0.bottom.equalToSuperview()
+        }
+    }
+
+    // MARK: - Helpers
+
+    private func setDelegate() {
+        resultCollectionView.dataSource = self
+        resultCollectionView.delegate = self
+    }
+
+    private func registerCell() {
+        resultCollectionView.register(
+            BookImageCell.self,
+            forCellWithReuseIdentifier: BookImageCell.identifier
+        )
+    }
+}
+
+// MARK: - UICollectionViewDataSource
+
+extension AddBookViewController: UICollectionViewDataSource {
+
+    func collectionView(
+        _ collectionView: UICollectionView,
+        numberOfItemsInSection section: Int
+    ) -> Int {
+        return 50
+    }
+
+    func collectionView(
+        _ collectionView: UICollectionView,
+        cellForItemAt indexPath: IndexPath
+    ) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: BookImageCell.identifier,
+            for: indexPath
+        ) as? BookImageCell else { return UICollectionViewCell() }
+
+        return cell
+    }
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout
+
+extension AddBookViewController: UICollectionViewDelegateFlowLayout {
+
+    func collectionView(
+        _ collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        sizeForItemAt indexPath: IndexPath
+    ) -> CGSize {
+        let width = (ScreenSize.width-36) / 3
+        return CGSize(width: width, height: 160)
+    }
+
+    func collectionView(
+        _ collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        minimumLineSpacingForSectionAt section: Int
+    ) -> CGFloat {
+        return 8
+    }
+
+    func collectionView(
+        _ collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        minimumInteritemSpacingForSectionAt section: Int
+    ) -> CGFloat {
+        return 8
     }
 }
