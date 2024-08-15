a65a52fb09ac3a4aa606cdd8113093e3bbb7c427
Mini <minnnidev@gmail.com>
2024-08-16 02:28:35 +0900

    [rename] Task -> APITask

--- BookTalk/BookTalk/Sources/Network/Base/APITask.swift
+++ BookTalk/BookTalk/Sources/Network/Base/APITask.swift
@@ -1,5 +1,5 @@
 //
-//  Task.swift
+//  APITask.swift
 //  BookTalk
 //
 //  Created by 김민 on 8/14/24.
@@ -8,7 +8,7 @@
 import Foundation
 import Alamofire
 
-enum Task {
+enum APITask {
     /// 추가 데이터가 없는 request
     case requestPlain
     /// Encodable을 채택하는 body를 포함하는 request
