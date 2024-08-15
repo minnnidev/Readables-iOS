36c49fdf26b1a370445bd9b5b8e44c2ea8c8c306
Mini <minnnidev@gmail.com>
2024-08-15 18:31:22 +0900

    [feat]  설정뷰 추가 및 화면 전환

---
+++ BookTalk/BookTalk/Sources/Presentation/MyPage/View/SettingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SettingViewController.swift
+//  BookTalk
+//
+//  Created by 김민 on 8/15/24.
+//
+
+import UIKit
+
+final class SettingViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .green
+    }
+}
