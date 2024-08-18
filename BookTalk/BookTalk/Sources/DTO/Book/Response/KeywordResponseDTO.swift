2dacafa22270d43e99593dd04904f3991fb5d058
Mini <minnnidev@gmail.com>
2024-08-19 03:51:01 +0900

    [add] 지난달 키워드 응답 DTO 추가

---
+++ BookTalk/BookTalk/Sources/DTO/Book/Response/KeywordResponseDTO.swift
@@ -0,0 +1,13 @@
+//
+//  KeywordResponseDTO.swift
+//  BookTalk
+//
+//  Created by 김민 on 8/19/24.
+//
+
+import Foundation
+
+struct KeywordResponseDTO: Decodable {
+    let keyword: String
+    let weight: String
+}
