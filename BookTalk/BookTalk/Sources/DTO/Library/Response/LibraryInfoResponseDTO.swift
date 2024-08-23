0c6f240049fe84456015de708fa7d879d6294fe8
Mini <minnnidev@gmail.com>
2024-08-23 19:22:31 +0900

    [fix] 책 검색 response dto 옵셔널 처리

--- BookTalk/BookTalk/Sources/DTO/Library/Response/LibraryInfoResponseDTO.swift
+++ BookTalk/BookTalk/Sources/DTO/Library/Response/LibraryInfoResponseDTO.swift
@@ -8,27 +8,27 @@
 import Foundation
 
 struct LibraryInfoResponseDTO: Decodable {
-    let libCode: String
-    let libName: String
-    let address: String
-    let tel: String
+    let libCode: String?
+    let libName: String?
+    let address: String?
+    let tel: String?
     let fax: String?
-    let latitude: String
-    let longitude: String
-    let homepage: String
-    let closed: String
-    let operatingTime: String
-    let bookCount: String
+    let latitude: String?
+    let longitude: String?
+    let homepage: String?
+    let closed: String?
+    let operatingTime: String?
+    let bookCount: String?
 }
 
 extension LibraryInfoResponseDTO {
 
     func toModel() -> LibraryInfo {
         return .init(
-            code: libCode,
-            name: libName,
-            address: address,
-            tel: tel
+            code: libCode ?? "",
+            name: libName ?? "도서관명 확인 불가",
+            address: address ?? "주소 확인 불가",
+            tel: tel ?? "번호 확인 불가"
         )
     }
 }
