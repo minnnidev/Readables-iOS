483b8c22ec9b8f8d9d78973fee4955364e1455fb
Mini <minnnidev@gmail.com>
2024-08-23 13:35:33 +0900

    [feat] 도서관 검색 api 통신을 위한 dto, service 구현

---
+++ BookTalk/BookTalk/Sources/DTO/Library/Response/LibraryInfoResponseDTO.swift
@@ -0,0 +1,33 @@
+//
+//  LibraryInfoResponseDTO.swift
+//  BookTalk
+//
+//  Created by 김민 on 8/23/24.
+//
+
+import Foundation
+
+struct LibraryInfoResponseDTO: Decodable {
+    let libCode: String
+    let libName: String
+    let address: String
+    let tel: String
+    let fax: String
+    let latitude: String
+    let longitude: String
+    let homepage: String
+    let closed: String
+    let operatingTime: String
+    let bookCount: String
+}
+
+extension LibraryInfoResponseDTO {
+
+    func toModel() -> LibraryInfo {
+        return .init(
+            name: libName,
+            address: address,
+            tel: tel
+        )
+    }
+}
---
+++ BookTalk/BookTalk/Sources/Model/Library/LibraryInfo.swift
@@ -0,0 +1,14 @@
+//
+//  LibraryInfo.swift
+//  BookTalk
+//
+//  Created by 김민 on 8/23/24.
+//
+
+import Foundation
+
+struct LibraryInfo {
+    let name: String
+    let address: String
+    let tel: String
+}
