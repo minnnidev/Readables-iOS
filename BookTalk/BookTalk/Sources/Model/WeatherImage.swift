36be703728da6537828b6180f8b027adeddc233f
RAFA <rafa71802937@gmail.com>
2024-08-29 16:36:42 +0900

    [feat] 날씨 배경이미지 모델 구현

---
+++ BookTalk/BookTalk/Sources/Model/WeatherImage.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherImage.swift
+//  BookTalk
+//
+//  Created by RAFA on 8/29/24.
+//
+
+import Foundation
+
+struct WeatherImage {
+    static let images = [
+        "cloudy", "dust", "foggy", "hail", "rain",
+        "snow", "sunFlurries", "sunny", "sunShowers", "thunder",
+        "tornado", "windy"
+    ]
+}
